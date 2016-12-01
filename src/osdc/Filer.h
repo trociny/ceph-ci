c336e86c2870f9606976f00a735b20315b606297
John Spray <john.spray@redhat.com>
2016-12-01 15:25:52 +0000

    osdc/Filer: const fix for passed layouts
    ...so that const references can be passed into
    purge calls.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/osdc/Filer.h
+++ src/osdc/Filer.h
@@ -227,7 +227,7 @@ class Filer {
   }
 
   void zero(inodeno_t ino,
-	   file_layout_t *layout,
+	   const file_layout_t *layout,
 	   const SnapContext& snapc,
 	   uint64_t offset,
 	   uint64_t len,
@@ -287,7 +287,7 @@ class Filer {
   }
   // purge range of ino.### objects
   int purge_range(inodeno_t ino,
-		  file_layout_t *layout,
+		  const file_layout_t *layout,
 		  const SnapContext& snapc,
 		  uint64_t first_obj, uint64_t num_obj,
 		  ceph::real_time mtime,
